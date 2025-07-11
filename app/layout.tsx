977e64a32d78526462c42e81d59dc601bcae460b
ferrydwinurrochman <ferrydn18@gmail.com>
2025-07-12 00:57:30 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Dashboard Shipment JNE',
+  description: 'Created by Marketing JNE Cirebon',
+  generator: 'Ferrydwinurrochman',
 }
 
 export default function RootLayout({
